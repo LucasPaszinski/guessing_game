12581c6a40b341633f6e06660ad1f5a895ff72f3
lucaspaszinski <lucaspaszinski@gmail.com>
2020-06-11 21:54:54 -0300

    Game with loop, and scape clause

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+
+    println!("Please input your guess: ");
+    println!("🤐 Secret is {} 🤐 ", secret_number);
+
+    loop {
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to Read the Line!");
+
+        let guess: u32 = guess.trim().parse().expect("Please Type a Number");
+        println!("You guessed: {}", guess);
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("🤩 YOU WIN ! 🎉🎊");
+                break;
+            },
+        }
+    }
 }
